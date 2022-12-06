09180f1c8b518640f269cf3222b62f27e23f5da2
danielsarasohn <59397382+danielsarasohn@users.noreply.github.com>
2022-12-05 20:13:42 -0500

    resolve issue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonMethods.java
@@ -94,7 +94,6 @@ public class AutonMethods {
         motorFR.setPower(0);
     }
 
-
     public long maps(long x, long in_min, long in_max, long out_min, long out_max)
     {
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
